2563e4cfb6f611ad7e8d99318b78d7207ef5c70f
Daniel Insley <dan@viviti.com>
2010-12-24 16:00:49 -0800

    removed old generator

--- generators/tartarus/templates/public/javascripts/tartarus.jquery.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-  $(function() {
-    $('.togglable').each(function() {
-      var toggle_data = $(this).find('.toggle_data');
-      $(this).find('.toggle_link').click(function() { toggle_data.toggle(); })
-    });
-  });
-})(jQuery);
--- generators/tartarus/templates/spec/models/logged_exception_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe <%= class_name %> do 
-  it "should include the Tartarus::Logger module" do
-    <%= class_name %>.included_modules.include?(Tartarus::Logger)
-  end
-end
