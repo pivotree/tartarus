38a1c6ab774ef41009dbb796a843adb06c50c606
Jerrett <jerrett@gmail.com>
2011-01-17 12:45:33 -0800

    prefix added, and fixed sender config

--- lib/tartarus/notifiers/mail.rb
+++ lib/tartarus/notifiers/mail.rb
@@ -1,8 +1,8 @@
 class Tartarus::Notifiers::Mail < ActionMailer::Base
   
   def notification(address, exception)
-    subject = "Exception raised at #{exception.controller_path}##{exception.action_name} (#{exception.exception_class}) #{exception.message}" 
-    mail(:to => address, :from => Tartarus.configuration['sender'], :subject => subject) do |format|
+    subject = "#{Tartarus.configuration['notification_prefix']}Exception raised at #{exception.controller_path}##{exception.action_name} (#{exception.exception_class}) #{exception.message}" 
+    mail(:to => address, :from => Tartarus.configuration['notification_sender'], :subject => subject) do |format|
       format.text do 
         render :text => %{
           A new exception was raised (#{exception.created_at.strftime("%m/%d/%Y %I:%M%p")}):
