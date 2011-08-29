45917f09f6d7b00c003d729e7dc4f1adde527dac
Brian Stolz <brian@tecnobrat.com>
2011-08-29 15:27:32 -0700

    Fix deprecation message.

--- lib/tartarus/railtie.rb
+++ lib/tartarus/railtie.rb
@@ -1,5 +1,5 @@
 class Tartarus::Railtie < Rails::Railtie
-  config.generators do |g|
+  config.app_generators do |g|
     g.orm             :activerecord, :migration => true
     g.template_engine :erb
     g.test_framework  :rspec
